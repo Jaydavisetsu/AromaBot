f144aa160b6917aef7c256ff7de6e1d2db7626da
autumnleaves410 <114082214+autumnleaves410@users.noreply.github.com>
2023-11-06 21:48:30 -0500

    Add files via upload

---
+++ Aroma.py
@@ -0,0 +1,11 @@
+'''
+Authors: Autumn Harrison & Jayden Davis
+Date: 2023-11-06
+Course: CSCI 2910 
+Document Name: Aroma.py
+
+This is the logic python file..?
+
+
+
+'''
---
+++ AromaMain.py
@@ -0,0 +1,11 @@
+'''
+Authors: Autumn Harrison & Jayden Davis
+Date: 2023-11-06
+Course: CSCI 2910 
+Document Name: AromaMain.py
+
+This is the main python file for the main method? 
+
+
+
+'''
