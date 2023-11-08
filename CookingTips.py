8b58fd8a00c74be7f39e3228b1f6de28e671f357
autumnleaves410 <114082214+autumnleaves410@users.noreply.github.com>
2023-11-07 21:04:04 -0500

    Add files via upload

---
+++ CookingTips.py
@@ -0,0 +1,15 @@
+'''
+Authors: Autumn Harrison & Jayden Davis
+Date: 2023-11-07
+Course: CSCI 2910 
+Document Name: CookingTips.py
+
+Contains all 50+ cooking tips that can be randomly chosen.
+
+'''
+
+cooking_tips = [
+    "Remember, y’all, it’s all about the prep. Take away the stress by doing the prep the night or day before. You’ll look like a star. - Paula Deen\n"
+    "The smaller the item, the higher the baking temperature. For example, I bake mini chocolate chip-toffee cookies at 500 degrees F for only 4 minutes. Perfect end result.\n"
+    
+]
